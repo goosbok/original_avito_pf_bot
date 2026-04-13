67b48de2109937f2aa7fcd2fb267f3836ef2d572
Demyan Belikov <d.belikov@skydns.ru>
2026-04-13 16:06:30 +0700

    Disabled reviews and seo

--- keyboards/users_menu.py
+++ keyboards/users_menu.py
@@ -31,18 +31,18 @@ def get_menu_kb():
             callback_data='tarifs:pf'
         )
     )
-    keyboard.add(
-        InlineKeyboardButton(
-            text=get_string('btn_reviews'),
-            callback_data="reviews"
-        )
-    ),
-    keyboard.add(
-        InlineKeyboardButton(
-            text=get_string('btn_seo_boost'),
-            callback_data="seo_boost"
-        )
-    )
+    # keyboard.add(
+    #     InlineKeyboardButton(
+    #         text=get_string('btn_reviews'),
+    #         callback_data="reviews"
+    #     )
+    # ),
+    # keyboard.add(
+    #     InlineKeyboardButton(
+    #         text=get_string('btn_seo_boost'),
+    #         callback_data="seo_boost"
+    #     )
+    # )
     keyboard.add(
         InlineKeyboardButton(
             text=get_string('btn_profile'),
